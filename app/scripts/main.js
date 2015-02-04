3f3b2be07317009f72ae8ceab35ebc61e06d01bd
Asa Toburen <atopettit@gmail.com>
2015-02-04 16:29:59 -0700

    added transitions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,3 +12,30 @@ function userScroll(ev){
 
 window.onscroll= userScroll;
 
+
+/* Typing Effect */
+
+function type(i, t, ie, oe) {
+    var input = document.getElementById(ie).innerHTML;
+    var inputOne = document.getElementById(oe);
+    document.getElementById(oe).innerHTML += input.charAt(i);
+    setTimeout(function(){
+        ((i < input.length - 1) ? type(i+1, t, ie, oe) : false);
+    }, t);
+}
+
+type(0, 75, "typing", "desktop-blank");
+
+
+
+
+var projectData = {
+	hotorcold : 'http://asatoburen.com/hot-or-cold-starter/',
+	musicdiscovery : 'http://asatoburen.com/last_fm',
+	stackoverflow : 'http://asatoburen.com/stackerAJAX',
+	startuplandingpage : 'http://asatoburen.comstartup-landing-page-clone',
+	streetfighter : 'http://asatoburen.com/jquerystreetfighter1',
+	waterquiz: 'http://asatoburen.com/quiz-app',
+	youtube_api: 'http://asatoburen.com/youtube_api'
+};
+
