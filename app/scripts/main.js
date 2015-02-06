d0ba2aae42a13d3dd2a5b22176e33e380621bf50
Asa Toburen <atopettit@gmail.com>
2015-02-06 09:55:12 -0700

    added portfolio section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,12 @@
 console.log('Hello!');
 
-var header = document.getElementById('headr');
 
-function userScroll(ev){
-	if(window.pageYOffset>575) {
-		header.classList.add('header-toggle');
-	} else if (window.pageYOffset<200){
-		header.classList.remove('header-toggle');
-	}
+function works() {
+    console.log('it\'s working');
 }
 
-window.onscroll= userScroll;
+new Vivus('phoneSvg', {type: 'async', duration: 200}, works);
+
 
 
 /* Typing Effect */
@@ -24,18 +20,23 @@ function type(i, t, ie, oe) {
     }, t);
 }
 
+
+var header = document.getElementById('headr');
+
+function userScroll(ev){
+    if(window.pageYOffset>50){
+        header.classList.add('header-fixed');
+    } else if (window.pageYOffset<100) {
+        header.classList.remove('header-fixed');
+    } else if(window.pageYOffset>575) {
+		header.classList.add('header-toggle');
+	} else if (window.pageYOffset<200){
+		header.classList.remove('header-toggle');
+	}
+}
+
+window.onscroll= userScroll;
+
+
+
 type(0, 75, "typing", "desktop-blank");
-
-
-
-
-var projectData = {
-	hotorcold : 'http://asatoburen.com/hot-or-cold-starter/',
-	musicdiscovery : 'http://asatoburen.com/last_fm',
-	stackoverflow : 'http://asatoburen.com/stackerAJAX',
-	startuplandingpage : 'http://asatoburen.comstartup-landing-page-clone',
-	streetfighter : 'http://asatoburen.com/jquerystreetfighter1',
-	waterquiz: 'http://asatoburen.com/quiz-app',
-	youtube_api: 'http://asatoburen.com/youtube_api'
-};
-
--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task('styles', function () {
 gulp.task('scripts', function () {
     return gulp.src('app/scripts/**/*.js')
         .pipe($.jshint())
-        .pipe($.jshint.reporter(require('jshint-stylish')))
+        //.pipe($.jshint.reporter(require('jshint-stylish')))
         .pipe($.size());
 });
 
